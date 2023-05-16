e4e715f45ce11275f89e30c16def6c4d7ee7add0
Gavin Birkhoff <gavinbirkhoff@gmail.com>
2023-05-16 22:47:09 +0800

    Update Side.jsx

--- src/layout/Side.jsx
+++ src/layout/Side.jsx
@@ -1,10 +1,24 @@
+import { useHistory } from "../hooks";
+
 const menus = [
-    {path:'/state', label: 'state'}
-]
-const Side = ()=>{
-    return <div>
-
+  { path: "/state", label: "state", name: "state" },
+  { path: "/id", label: "id", name: "id" },
+];
+const Side = () => {
+  const history = useHistory();
+  const handleClick = (item) => {
+    const { path } = item;
+    history.pushState({}, null, path);
+  };
+  return (
+    <div>
+      <ul>
+        {menus.map((menu) => (
+          <li onClick={() => handleClick(menu)}>{menu.label}</li>
+        ))}
+      </ul>
     </div>
-}
+  );
+};
 
-export default Side
+export default Side;
