c59558e978d7c9809e118421374ef442ea8ec5ba
Gavin Birkhoff <gavinbirkhoff@gmail.com>
2023-05-18 23:51:11 +0800

    路由切换刷新页面

--- src/layout/BaseLayout.jsx
+++ src/layout/BaseLayout.jsx
@@ -1,11 +1,15 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import Fixtures from "./Fixtures";
 import Side from "./Side";
+import { useLatest } from "../hooks";
 
 const BaseLayout = () => {
+  const [historyCount, setHistoryCount] = useState(0);
+  const historyCountRef = useLatest(historyCount)
   useEffect(() => {
     const pathChange = (...e) => {
-        console.log(...e);
+      console.log(...e);
+      setHistoryCount(historyCountRef.current + 1);
     };
     window.addEventListener("popstate", pathChange);
     return () => {
